14cd307f1f0c6fe50e2a96849bc6fed821a57094
Bruce Frank Wong <bruce.frank.wong@gmail.com>
2021-08-15 21:09:33 +0800

    Modify script <install-python2.sh>.

--- install-python2.sh
+++ install-python2.sh
@@ -4,9 +4,16 @@
 
 VERSION="2.7.18"
 
+
+VER=$(echo ${VERSION} | awk 'BEGIN{FS="."} {print $1"."$2}')
+
+
+# --------------------------------------------------
 # Update the package and install the requirements.
+# --------------------------------------------------
 sudo apt-get update && sudo apt-get install -y \
     build-essential \
+    tk-dev \
     zlib1g-dev \
     libncurses5-dev \
     libgdbm-dev \
@@ -17,39 +24,64 @@ sudo apt-get update && sudo apt-get install -y \
     libffi-dev \
     libbz2-dev
 
+
+# --------------------------------------------------
 # Create the temporary directory.
+# --------------------------------------------------
 if [ ! -d ${HOME}/temp ]; then
     mkdir ${HOME}/temp
 fi
 
+
+# --------------------------------------------------
 # Download the source code.
+# --------------------------------------------------
 cd ${HOME}/temp
 wget https://www.python.org/ftp/python/${VERSION}/Python-${VERSION}.tgz
 tar -xvf Python-${VERSION}.tgz
 
+
+# --------------------------------------------------
 # Compile the source code and install.
+# --------------------------------------------------
 cd Python-${VERSION}
 ./configure --enable-optimizations
 sudo make
 sudo make altinstall
 
+
+# --------------------------------------------------
 # Check whether Python 2.7.x install succeed.
-if [ ! -x /usr/local/bin/python2.7 ]; then
+# --------------------------------------------------
+if [ ! -x /usr/local/bin/python${VER} ]; then
     echo "ERROR! Python ${VERSION} is not install correctly."
+    cd ${HOME}
+    sudo rm -rf ${HOME}/temp/Python-${VERSION}
+    rm ${HOME}/temp/Python-${VERSION}.tgz
     exit 1
 fi
 
+
+# --------------------------------------------------
 # Download pip
-curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o ${HOME}/temp/get-pip.py
-/usr/local/bin/python2.7 ${HOME}/temp/get-pip.py
+# --------------------------------------------------
+cd ${HOME}/temp
+wget https://bootstrap.pypa.io/pip/${VER}/get-pip.py
+/usr/local/bin/python${VER} get-pip.py
 
+
+# --------------------------------------------------
 # Check whether Python 2.7.x install succeed.
-# if [ ! -x /usr/local/bin/pip2.7 ]; then
-#     echo "ERROR! Python ${VERSION} is not install correctly."
-#     exit 1
-# fi
+# --------------------------------------------------
+if [ ! -x ${HOME}/.local/bin/pip${VER} ]; then
+    echo "ERROR! PIP ${VER} is not install correctly."
+    exit 1
+fi
 
+
+# --------------------------------------------------
 # Make clean.
+# --------------------------------------------------
 sudo rm -rf ${HOME}/temp/Python-${VERSION}
 rm ${HOME}/temp/Python-${VERSION}.tgz
 rm ${HOME}/temp/get-pip.py
